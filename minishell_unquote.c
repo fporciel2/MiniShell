7163283c35bbeb32f629119e82ee7d434e6fcea7
fporciel2 <124692932+fporciel2@users.noreply.github.com>
2024-04-04 12:32:10 +0200

    Thu  4 Apr 12:32:07 CEST 2024

---
+++ minishell_unquote.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_unquote.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 12:31:56 by fporciel          #+#    #+#             */
+/*   Updated: 2024/04/04 12:32:02 by fporciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/* <one line to give the program's name and a brief idea
+ * of what it does.>
+ * Copyright (C) <year>  <name of author>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * For more information on how to contact me by electronic and paper mail
+ * please see:
+ * <your contact information here>
+ */
+
+#include "minishell.h"
