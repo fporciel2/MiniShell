37bc9d202586643b945dce2f9af95d68e146e1ad
fporciel2 <124692932+fporciel2@users.noreply.github.com>
2024-04-29 16:26:02 +0200

    Mon 29 Apr 16:25:58 CEST 2024

--- minishell.c
+++ minishell.c
@@ -1,62 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 16:12:09 by fporciel          #+#    #+#             */
-/*   Updated: 2024/04/29 16:20:49 by fporciel         ###   ########.fr       */
+/*   Created: 2024/04/29 16:15:05 by fporciel          #+#    #+#             */
+/*   Updated: 2024/04/29 16:18:23 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
- * `MiniShell` is a simple shell for Debian GNU/Linux.
+/* `MiniShell` is a simple shell for Debian GNU/Linux.
  * Copyright (C) 2024 Federico Porciello
- *
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
  * For more information on how to contact me by electronic and paper mail
  * please see:
  * https://github.com/fporciel2/MiniShell
  */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# define _DEFAULT_SOURCE
-# define _POSIX_C_SOURCE
-# define BAD_START "minishell doesn't take arguments yet"
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <sys/wait.h>
-# include <sys/time.h>
-# include <sys/resource.h>
-# include <signal.h>
-# include <dirent.h>
-# include <string.h>
-# include <errno.h>
-# include <sys/ioctl.h>
-# include <termios.h>
-# include <curses.h>
-# include <term.h>
+#include "minishell.h"
 
-/* PRINT ERRORS */
-int	msh_error(char *str1, char *str2, char *str3);
-
-#endif
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc != 1)
+		return (msh_error(BAD_START, NULL, NULL));
+}
